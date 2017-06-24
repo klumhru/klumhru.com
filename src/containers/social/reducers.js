20ee4fb7ed279fd77c42299022ce5132ad4b26cc
Högni Gylfason <klumhru@gmail.com>
2017-06-24 12:54:18 +0000

    Installed and configured redux

---
+++ src/containers/social/reducers.js
@@ -0,0 +1,13 @@
+const socialState = {
+  github: {
+    url: 'https://github.com/Klumhru',
+  },
+}
+
+export const social = (state = socialState, action) => {
+  switch (action.type) {
+    default: {
+      return state
+    }
+  }
+}
